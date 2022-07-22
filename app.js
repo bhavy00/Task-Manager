d29d8f59dc4439af0ce1d48221893b35ac4fc2dc
dogeshvar <bhavy02@gmail.com>
2022-07-22 21:14:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes/task')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+// middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+// routes
+app.get('/hello', (req, res)=>{
+    res.send("task manager app")
+})
+
+app.use('/api/v1/tasks', routes)
+
+
+const port = 3000;
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}`))
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+start()
